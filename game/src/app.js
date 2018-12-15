4d8741c78102cc3ead3c4ec44baa68ff21518e53
uniorunr <gagael23@live.com>
2018-12-16 01:06:43 +0300

    refactor: use class syntax for game character declaration

--- game/src/app.js
+++ game/src/app.js
@@ -1,76 +1,100 @@
-const canvasWidth = ((window.innerWidth / 10).toFixed(0) - 3) * 10;
-const canvasHeight = ((window.innerHeight / 10).toFixed(0) - 3) * 10 - 20;
-
 const canvas = document.getElementById('myCanvas');
-canvas.width = canvasWidth;
-canvas.height = canvasHeight;
+canvas.width = ((window.innerWidth / 10).toFixed(0) - 3) * 10;
+canvas.height = ((window.innerHeight / 10).toFixed(0) - 3) * 10 - 20;
 const ctx = canvas.getContext('2d');
 
-const spriteWidth = 960;
-const spriteHeight = 960;
-
-const rows = 10;
-const cols = 10;
-
-const width = spriteWidth / cols;
-const height = spriteHeight / rows;
-
-let curFrame = 0;
-const frameCount = 10;
-
-let srcX = 0;
-let srcY = 0;
-
-const fps = 6;
-
-let anim = 'idle';
-
-const character = new Image();
-
-character.src = '../img/wizard_sprite.png';
-
-function updateFrame() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  curFrame = (curFrame += 1) % frameCount;
-  srcX = curFrame * width;
-}
-
-function draw() {
-  updateFrame();
-  ctx.drawImage(character, srcX, srcY, width, height,
-    canvasWidth / 2 - width / 2, canvasHeight / 2 - height / 2, width, height);
-  if (curFrame === 9 && anim !== 'idle') {
-    srcY = 0;
+class Canvas {
+  constructor() {
+    this.canvas = canvas;
+    this.ctx = ctx;
   }
 }
 
-function attack() {
-  curFrame = 0;
-  srcY = 3 * height;
-  anim = 'attack';
+class Sprite extends Canvas {
+  constructor(name, hp, sWidth, sHeight, rows, cols, curFrame, frames, spriteSrc) {
+    super(canvas, ctx);
+    this.name = name;
+    this.hp = hp;
+    this.spriteWidth = sWidth;
+    this.spriteHeight = sHeight;
+    this.spriteRows = rows;
+    this.spriteCols = cols;
+    this.curFrame = curFrame;
+    this.frames = frames;
+    this.sprite = new Image();
+    this.sprite.src = spriteSrc;
+  }
+
+  frameWidth() {
+    return this.spriteWidth / this.spriteCols;
+  }
+
+  frameHeight() {
+    return this.spriteHeight / this.spriteRows;
+  }
 }
 
-function heal() {
-  curFrame = 0;
-  srcY = height;
-  anim = 'heal';
+class Character extends Sprite {
+  constructor(name, hp, sWidth, sHeight, rows, cols,
+    curFrame, frames, spriteSrc, srcX, srcY, fps, curAction, position, dPos) {
+    super(name, hp, sWidth, sHeight, rows, cols, curFrame, frames, spriteSrc);
+    this.srcX = srcX;
+    this.srcY = srcY;
+    this.fps = fps;
+    this.curAction = curAction;
+    this.position = position;
+    this.dPos = dPos;
+  }
+
+  updateFrame() {
+    this.ctx.clearRect(canvas.width * this.position - this.frameWidth() / 2,
+      canvas.height / 2 - this.frameHeight() * this.dPos, this.frameWidth(), this.frameHeight());
+    this.curFrame = (this.curFrame += 1) % this.frames;
+    this.srcX = this.curFrame * this.frameWidth();
+  }
+
+  draw() {
+    this.updateFrame();
+    this.ctx.drawImage(this.sprite, this.srcX, this.srcY, this.frameWidth(), this.frameHeight(),
+      canvas.width * this.position - this.frameWidth() / 2,
+      canvas.height / 2 - this.frameHeight() * this.dPos,
+      this.frameWidth(), this.frameHeight());
+    if (this.curFrame === this.frames - 1 && this.curAction !== 'idle') {
+      this.srcY = 0;
+    }
+  }
+
+  heal() {
+    this.curFrame = 0;
+    this.srcY = this.frameHeight();
+    this.curAction = 'heal';
+  }
+
+  walk() {
+    this.curFrame = 0;
+    this.srcY = 2 * this.frameHeight();
+    this.curAction = 'walk';
+  }
+
+  attack() {
+    this.curFrame = 0;
+    this.srcY = 3 * this.frameHeight();
+    this.curAction = 'attack';
+  }
+
+  death() {
+    this.curFrame = 0;
+    this.srcY = 4 * this.frameHeight();
+    this.curAction = 'death';
+  }
 }
 
-function walk() {
-  curFrame = 0;
-  srcY = 2 * height;
-  anim = 'walk';
-}
+const hero = new Character('Piggy', 100, 1280, 1280,
+  10, 10, 0, 10, '../img/wizard_sprite.png', 0, 0, 6, 'idle', 0.25, 0.5);
 
-function death() {
-  curFrame = 0;
-  srcY = 4 * height;
-  anim = 'death';
-}
+setInterval(hero.draw.bind(hero), 1000 / hero.fps);
 
-setInterval(draw, 1000 / fps);
-
-document.querySelector('.control.attack').addEventListener('click', attack);
-document.querySelector('.control.heal').addEventListener('click', heal);
-document.querySelector('.control.walk').addEventListener('click', walk);
-document.querySelector('.control.death').addEventListener('click', death);
+document.querySelector('.control.attack').addEventListener('click', hero.attack.bind(hero));
+document.querySelector('.control.heal').addEventListener('click', hero.heal.bind(hero));
+document.querySelector('.control.walk').addEventListener('click', hero.walk.bind(hero));
+document.querySelector('.control.death').addEventListener('click', hero.death.bind(hero));
