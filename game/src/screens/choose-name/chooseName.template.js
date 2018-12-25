32aaf0478f07d266eed452ca0dfe95acb62310cf
Yury Chuchvaha <gagael23@live.com>
2018-12-25 12:02:03 +0300

    fix: add checker for input to allow english only

--- game/src/screens/choose-name/chooseName.template.js
+++ game/src/screens/choose-name/chooseName.template.js
@@ -2,8 +2,9 @@ export default `
 <section id="choosePlayerName" class="choose-player-name">
   <form class="choose-name-form">
   <h1>Hey kid, choose your magic name!</h1>
-  <label>magic name:<input type="text" class="player-name" required="" maxlength="15"></label>
+  <label>magic name:<input type="text" class="player-name" required="" maxlength="15" pattern="[a-zA-Z0-9]+"></label>
   <input type="submit" class="submit-button" value="->"></form>
+  <p class="only-english">(only english letters allowed)</p>
   <input type="button" class="close" value="x">
 </section>
 `;
