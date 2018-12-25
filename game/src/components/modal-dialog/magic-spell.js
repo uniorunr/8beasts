a23d8f05d55cd04a5021e8613c112445e958cde5
Yury Chuchvaha <gagael23@live.com>
2018-12-25 14:33:39 +0300

    feature: add static method empty to MagicSpell class

--- game/src/components/modal-dialog/magic-spell.js
+++ game/src/components/modal-dialog/magic-spell.js
@@ -5,6 +5,10 @@ class MagicSpell {
     const element = document.querySelector('.container');
     element.insertAdjacentHTML('afterbegin', template);
   }
+
+  static empty() {
+    document.querySelector('#magicSpell').remove();
+  }
 }
 
 export default MagicSpell;
